32461e4b0b70ce0e513b9c7a03c4e1622bf1035d
novicasarenac <nolesarenac@yahoo.com>
2017-04-24 02:33:04 +0100

    Messages added

---
+++ UserAppEJB/ejbModule/jms_messages/UserRequestMessage.java
@@ -0,0 +1,47 @@
+package jms_messages;
+
+import java.io.Serializable;
+
+import model.Host;
+
+public class UserRequestMessage implements Serializable {
+
+	private String username;
+	private String password;
+	private Host host;
+	private UserRequestMessageType type;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Host getHost() {
+		return host;
+	}
+	public void setHost(Host host) {
+		this.host = host;
+	}
+	public UserRequestMessageType getType() {
+		return type;
+	}
+	public void setType(UserRequestMessageType type) {
+		this.type = type;
+	}
+	public UserRequestMessage() {
+		super();
+	}
+	public UserRequestMessage(String username, String password, Host host, UserRequestMessageType type) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.host = host;
+		this.type = type;
+	}
+}
